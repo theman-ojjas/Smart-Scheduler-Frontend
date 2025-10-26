f10cd91b11ac90ae1292108ba92e30873a8d4d20
theman-ojjas <123209996+theman-ojjas@users.noreply.github.com>
2025-10-26 05:36:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function App() {
       }));
 
       // Post request
-      const res = await axios.post(
-        `${process.env.REACT_APP_API_URL}/api/v1/projects/1/schedule`,
+       const res = await axios.post(
+        "https://smart-scheduler-backend.onrender.com/api/v1/projects/1/schedule",
         { tasks: formattedTasks, workHoursPerDay }
       );
 
